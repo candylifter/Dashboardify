9d20ca18e15910628aa750206a715484dddf7058
Martynas Kadiša <kadisa.martynas@gmail.com>
2016-08-12 11:38:26 +0300

    Add create user in SQL script

--- DataBase/DashBoardifyDBScript.sql
+++ DataBase/DashBoardifyDBScript.sql
@@ -2,8 +2,14 @@ CREATE DATABASE DashBoardify;
 
 GO
 USE DashBoardify
+GO
 
+CREATE LOGIN [DashboardifyUser]
+WITH PASSWORD = '123456';
 
+CREATE USER [DashboardifyUser] FOR LOGIN [DashboardifyUser];
+ALTER ROLE [db_owner] ADD MEMBER [DashboardifyUser];
+GO
 
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Items')
 BEGIN
