5f3e699d6800507e2570eda32f4846d76be7b158
Martynas Kadiša <kadisa.martynas@gmail.com>
2016-08-23 12:15:17 +0300

    Add React, Webpack. Remove gulp, bower, backbone

---
+++ Dashboardify/Dashboardify.Web/src/app/components/DashboardifyApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DashboardifyApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Dashboardify App</h1>
+      </div>
+    )
+  }
+}
+
+export default DashboardifyApp;
