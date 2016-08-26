655f9a9aadb6040d1a168b6ee5c9617a38927e58
Žygimantas Žilevičius <zygimantas.zilevicius@gmail.com>
2016-08-26 16:50:58 +0300

    Small Changes ir Repos

--- Dashboardify/Dashboardify.Repositories/ScreenshotRepository.cs
+++ Dashboardify/Dashboardify.Repositories/ScreenshotRepository.cs
@@ -1,53 +1,65 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Data;
-//using System.Data.SqlClient;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Dapper;
-//using Dashboardify.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Dashboardify.Models;
 
-//namespace Dashboardify.Repositories
-//{
-//    class ScreenshotRepository
-//    {
-//        private string _connectionString;
+namespace Dashboardify.Repositories
+{
+    class ScreenshotRepository
+    {
+        private string _connectionString;
 
-//        public ScreenshotRepository(string connectionString)
-//        {
-//            this._connectionString = connectionString;
-//        }
+        public ScreenshotRepository(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
 
-//        public Screenshot GetLast(int itemId)
-//        {
-//            try
-//            {
-//                using (IDbConnection db = new SqlConnection(_connectionString))
-//                {
-//                    return db.Query<User>(@"
-//                            SELECT 
-//                                Id,
-//                                Name,
-//                                Password,
-//                                Email,
-//                                IsActive,
-//                                DateRegistered,
-//                                DateModified
-//                            FROM 
-//                                Users 
-//                            WHERE 
-//                                Id = @Id", new { id }).SingleOrDefault();
+        public Screenshot GetLast(int itemId)
+        {
+            try
+            {
+                using (IDbConnection db = new SqlConnection(_connectionString))
+                {
+                    return db.Query<User>(@"
+                            SELECT 
+                                Id,
+                                Name,
+                                Password,
+                                Email,
+                                IsActive,
+                                DateRegistered,
+                                DateModified
+                            FROM 
+                                Users 
+                            WHERE 
+                                Id = @Id", new { id }).SingleOrDefault();
 
-//                }
-//            }
-//            catch (Exception ex)
-//            {
-//                Console.WriteLine(ex.Message);
-//                throw;
-//            }
-            
-//        }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
 
-//    }
-//}
+        }
+
+        public IList<Screenshot> GetAll()
+        {
+            string query = @"SELECT
+                                Id, 
+                                ItemId,
+                                ScrnshtURL,
+                                DateTaken
+                            FROM 
+                                ScreenShots
+                            WHERE Id=" + "";
+        }
+
+    }
+}
