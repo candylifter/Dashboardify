76dbd038cd4a5e62c3e165176f26e166ea35175e
Martynas Kadiša <kadisa.martynas@gmail.com>
2016-08-30 10:40:23 +0300

    Add Items controller and remove AI from WebAPI

---
+++ Dashboardify/Dashboardify.WebApi/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Dashboardify.WebApi.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            return Json(true);
+        }
+
+    }
+}
