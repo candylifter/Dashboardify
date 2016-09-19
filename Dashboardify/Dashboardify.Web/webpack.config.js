279d9d7b4162236f31c1a88a3fce4f8c5b9d92f7
Martynas Kadiša <kadisa.martynas@gmail.com>
2016-09-19 16:42:50 +0300

    Remove ASP.NET MVC website, add nodejs website

--- Dashboardify/Dashboardify.Web/webpack.config.js
+++ Dashboardify/Dashboardify.Web/webpack.config.js
@@ -1,71 +1,65 @@
-var webpack = require('webpack')
 var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
+var config = {
+  devtool: 'eval-source-map',
   entry: [
-    'script!jquery/dist/jquery.min.js',
-    './app/app.jsx'
-  ],
-  externals: {
-    jquery: 'jQuery'
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      '$': 'jquery',
-      'jQuery': 'jquery'
-    })
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'app/app.jsx')
   ],
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: './bundle.js'
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'react-hmre'],
+        plugins: ['transform-object-rest-spread']
+      }
+    }, {
+      test: /\.json?$/,
+      loader: 'json'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+    }]
   },
   resolve: {
     root: __dirname,
     modulesDirectories: [
-      'node_modules',
-      './app/api'
+      'node_modules'
     ],
     alias: {
       applicationStyles: 'app/styles/app.scss',
-      DashboardifyAPI: 'app/api/DashboardifyAPI.jsx',
-      api: 'app/api/index.jsx',
       components: 'app/components/index.jsx',
       containers: 'app/containers/index.jsx',
       actions: 'app/actions/index.jsx',
       reducers: 'app/reducers/index.jsx',
-      configureStore: 'app/store/configureStore.jsx',
+      configureStore: 'app/store/configureStore',
+      api: 'app/api/index.jsx',
       routes: 'app/routes.jsx'
     },
     extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015'],
-          plugins: ['transform-object-rest-spread']
-        },
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
-      }
-    ]
-  },
-  sassLoader: {
-    includePaths: [
-      path.resolve(__dirname, './node_modules/bootstrap-sass/assets/stylesheets'),
-      path.resolve(__dirname, './node_modules/bootstrap-sass'),
-      path.resolve(__dirname, './node_modules/bourbon/app/assets/stylesheets/'),
-      path.resolve(__dirname, './node_modules/bourbon-neat/app/assets/stylesheets/')
-    ]
-  },
-  devtool: 'eval-source-map'
+  }
 }
+
+module.exports = config
