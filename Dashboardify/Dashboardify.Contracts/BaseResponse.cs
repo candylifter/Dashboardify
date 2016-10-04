a6069ce889f8c81aa6a21e3b677d7ab8cca05ac6
Žygimantas Žilevičius <zygimantas.zilevicius@gmail.com>
2016-10-04 13:34:25 +0300

    Modified API, added security providers

--- Dashboardify/Dashboardify.Contracts/BaseResponse.cs
+++ Dashboardify/Dashboardify.Contracts/BaseResponse.cs
@@ -4,10 +4,7 @@ namespace Dashboardify.Contracts
 {
     public class BaseResponse
     {
-        public bool HasErrors
-        {
-            get { return Errors.Count > 0; }
-        }
+        public bool HasErrors => Errors.Count > 0;
 
         public IList<ErrorStatus> Errors { get; set; }
 
--- Dashboardify/Dashboardify.WebApi/Controllers/DashboardsController.cs
+++ Dashboardify/Dashboardify.WebApi/Controllers/DashboardsController.cs
@@ -1,46 +1,86 @@
 ﻿using System.Net.Http;
 using System.Web.Http;
 using System.Configuration;
+using Dashboardify.Contracts;
 using Dashboardify.Contracts.Dashboards;
 using Dashboardify.Handlers.Dashboards;
+using Dashboardify.WebApi.SecurityProvider;
 
 
 namespace Dashboardify.WebApi.Controllers
 {
     public class DashboardsController : BaseController
     {
-        private static string _connectionString = ConfigurationManager.ConnectionStrings["GCP"].ConnectionString;
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["GCP"].ConnectionString;
         
         
         [HttpPost]
-        public HttpResponseMessage GetList(GetDashboardsRequest request)
+        public HttpResponseMessage GetList(BaseRequest request)
         {
-            //var securityProvider = new SecurityProvider.SecurityProvider(request);
 
-            //securityProvider.DoValidation();
+            var response = new BaseResponse();
 
-            //var errors = securityProvider.ReturnErrors();
+            var securityProvider = new DashSecurityProvider(request);
+            
+            response = securityProvider.CheckForAnyBaseErrors();
 
-            //if (errors.Count > 0)
-            //{
-                
-            //}
+            if (response.HasErrors)
+            {
+                var statusCodeFail = ResolveStatusCode(response);       //nezinau ar geriausias sprendimas, bet PX, veikia, net peles neatitrauki
+
+                return Request.CreateResponse(statusCodeFail,response);
+            }
+            
+            var user = securityProvider.GetUser();
+
+            var GetDashBoardsRequest = new GetDashboardsRequest{ Id = user.Id};
 
             var handler = new GetDashboardsHandler(_connectionString);
 
-            var response = handler.Handle(request);
+            response = handler.Handle(GetDashBoardsRequest);
 
             var statusCode = ResolveStatusCode(response);
 
             return Request.CreateResponse(statusCode, response);
+            
+
+            
         }
 
         [HttpPost]
         public HttpResponseMessage Update(UpdateDashboardRequest request)
         {
+            var response = new BaseResponse();
+
+            var securityProvider = new DashSecurityProvider(request);
+
+            response = securityProvider.CheckForAnyBaseErrors();
+
+            var authentificationResponse = new UpdateDashboardResponse();
+
+            authentificationResponse = securityProvider.CheckForAuthentificationErrors(request);
+
+            if (response.HasErrors)
+            {
+                var statusCodeFail = ResolveStatusCode(response);
+
+                return Request.CreateResponse(statusCodeFail, response);
+            }
+            if (authentificationResponse.HasErrors)
+            {
+                var statusCodeFail = ResolveStatusCode(authentificationResponse);
+
+                return Request.CreateResponse(statusCodeFail, authentificationResponse);
+            }
+            
+            var updateDashBoardRequest = new UpdateDashboardRequest
+            {
+                DashBoard = request.DashBoard
+            };
+
             var handler = new UpdateDashBoardHandler(_connectionString);
 
-            var response = handler.Handle(request);
+            response = handler.Handle(updateDashBoardRequest);
 
             var statusCode = ResolveStatusCode(response);
 
