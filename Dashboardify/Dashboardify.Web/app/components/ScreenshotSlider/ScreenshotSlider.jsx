f69f7f9e0998720d29543a05756abd4e7c39f037
Martynas Kadiša <kadisa.martynas@gmail.com>
2016-10-04 17:06:22 +0300

    only show previous screenshots if length is greater than 1

--- Dashboardify/Dashboardify.Web/app/components/ScreenshotSlider/ScreenshotSlider.jsx
+++ Dashboardify/Dashboardify.Web/app/components/ScreenshotSlider/ScreenshotSlider.jsx
@@ -7,16 +7,26 @@ class ScreenshotSlider extends React.Component {
     let { screenshots } = this.props
 
     let renderScreenshots = () => {
-      return screenshots.map((screenshot, index) => {
+      if (screenshots.length > 1) {
+        return screenshots.map((screenshot, index) => {
+          if (index !== 0) {
+            return (
+              <Paper key={index} className='screenshot-slider__item'>
+                <img src={screenshot.ScrnshtURL} alt='Older screenshot' className='screenshot-slider__item__image' />
+                <div className='screenshot-slider__item__header'>
+                  <span>Screenshot taken {moment(screenshot.DateTaken).fromNow()}</span>
+                </div>
+              </Paper>
+            )
+          }
+        })
+      } else {
         return (
-          <Paper key={index} className='screenshot-slider__item'>
-            <img src={screenshot.ScrnshtURL} alt='Older screenshot' className='screenshot-slider__item__image' />
-            <div className='screenshot-slider__item__header'>
-              <span>Screenshot taken {moment(screenshot.DateTaken).fromNow()}</span>
-            </div>
-          </Paper>
+          <div>
+            <h4>There are no older screenshots</h4>
+          </div>
         )
-      })
+      }
     }
 
     return (
